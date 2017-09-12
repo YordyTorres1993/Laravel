66c4182aea971e5afc35e07d33f0c929a6c0e39d
Yordy Torres Rojas <ingtorresrojas@gmail.com>
2017-09-12 18:16:16 -0500

    Installation of Laravel and Dependencies

--- SisVentas1/config/app.php
+++ SisVentas1/config/app.php
@@ -171,11 +171,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        sisVentas\Providers\AppServiceProvider::class,
+        sisVentas\Providers\AuthServiceProvider::class,
+        // sisVentas\Providers\BroadcastServiceProvider::class,
+        sisVentas\Providers\EventServiceProvider::class,
+        sisVentas\Providers\RouteServiceProvider::class,
 
     ],
 
